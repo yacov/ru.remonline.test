6783638f56169261b5813377e8f2e324342b3f62
Anasteisha <anaskazar@gmail.com>
2014-08-25 18:09:57 +0300

    Auto test for setting company
    Positive auto test for setting company

---
+++ src/test/java/ru/telran/sel/OptionClassTest.java
@@ -0,0 +1,54 @@
+package ru.telran.sel;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.testng.*;
+import org.testng.annotations.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+public class OptionClassTest extends ru.telran.sel.pages.TestBase {
+private boolean acceptNextAlert = true;
+private StringBuffer verificationErrors = new StringBuffer () ;
+@Test
+public void testOption () throws Exception {    
+	
+	openPage();
+	clickToLogin();    
+    fillLoginForm(new LoginData("Nastya3", "qwerty123456"));    
+    clickOnEnter();
+   
+    clickMyCompany();
+    clickSetting();
+    fillSettingForm();
+    clickSave();
+    try {
+      assertEquals("Успешно сохранено", driver.findElement(By.xpath("//div[5]")).getText());
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+        
+    clickToExit();
+ }
+private void clickSave() {
+	driver.findElement(By.xpath("//div[@id='container']/div/div/div/div[2]/button")).click();
+}
+private void fillSettingForm() {
+    
+    driver.findElement(By.id("l-settings-name")).clear();
+    driver.findElement(By.id("l-settings-name")).sendKeys("My company");
+    driver.findElement(By.id("l-settings-address")).clear();
+    driver.findElement(By.id("l-settings-address")).sendKeys("Tartar");
+    new Select(driver.findElement(By.id("js-settings-country"))).selectByVisibleText("Молдова");
+	
+}
+private void clickSetting() {
+	driver.findElement(By.xpath(".//*[@id='container']//ul[@class='nav nav-tabs h-mb-20']//a[@href='#!/company/settings']")).click();
+}
+private void clickMyCompany() {
+	driver.findElement(By.xpath("//a[@href='#!/company']")).click();
+}
+
+}
