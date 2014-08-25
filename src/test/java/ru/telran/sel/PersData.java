b10f30f6ed88aa8d24c44628a028847a6a5247d1
dashik1988 <daria_sergeevna8@mail.ru>
2014-08-25 23:55:42 +0200

    Classes for testing add employee
    i just copyed classes files

---
+++ src/test/java/ru/telran/sel/PersData.java
@@ -0,0 +1,22 @@
+package ru.telran.sel;
+
+public class PersData {
+	public String username;
+	public String login;
+	public String pass;
+	public String phone;
+	public String mail;
+	public String selectVal;
+	public String percent;
+
+	public PersData(String username, String login, String pass, String phone,
+			String mail, String selectVal, String percent) {
+		this.username = username;
+		this.login = login;
+		this.pass = pass;
+		this.phone = phone;
+		this.mail = mail;
+		this.selectVal = selectVal;
+		this.percent = percent;
+	}
+}
