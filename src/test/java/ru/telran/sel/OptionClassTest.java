0fbe7595c2896f625ff79cebc52e79c343b36c2f
AlexSnear <akazarikin@gmail.com>
2014-08-26 09:30:48 +0300

    Try to set repository

--- src/test/java/ru/telran/sel/OptionClassTest.java
+++ src/test/java/ru/telran/sel/OptionClassTest.java
@@ -12,6 +12,7 @@ import org.openqa.selenium.support.ui.Select;
 public class OptionClassTest extends ru.telran.sel.pages.TestBase {
 private boolean acceptNextAlert = true;
 private StringBuffer verificationErrors = new StringBuffer () ;
+//try set repository
 @Test
 public void testOption () throws Exception {    
 	
