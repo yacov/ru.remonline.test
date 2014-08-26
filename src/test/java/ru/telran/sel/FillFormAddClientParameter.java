42ef59738b419e852deaf9b1b8da5196f6643198
FrenkyFix1987 <frankyfix6@gmail.com>
2014-08-26 11:31:41 +0300

    Classes for testing add client

---
+++ src/test/java/ru/telran/sel/FillFormAddClientParameter.java
@@ -0,0 +1,16 @@
+package ru.telran.sel;
+
+public class FillFormAddClientParameter {
+	public String name;
+	public String telephon;
+	public String email;
+	public String adress;
+
+	public FillFormAddClientParameter(String name, String telephon,
+			String email, String adress) {
+		this.name = name;
+		this.telephon = telephon;
+		this.email = email;
+		this.adress = adress;
+	}
+}
